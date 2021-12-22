b56ed742c6c4a614b504a03a90396b8cc2f51552
hayato0309 <hayato.pluto.4538@gmail.com>
2021-12-22 21:56:54 +0900

    create backend for streak section

---
+++ update_day_record.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__FILE__) . '/connect.php';
+require_once dirname(__FILE__) . '/classes/DayRecord.php';
+
+$dateStart = $_POST['dateStart'];
+$today = date('Y-m-d');
+$dayRecord = new DayRecord($dateStart);
+
+if ($_POST['submit'] === 'submit' && $dateStart !== '' && $dayRecord->isDateStartOnOrBeforeToday($dateStart, $today)) {
+    // submitボタンが押されている、かつ、日付が選択されている、かつ、日付が今日以前のとき
+    $statement = $pdo->prepare('UPDATE day_record SET date_Start = :dateStart, created_at = CURRENT_TIMESTAMP');
+    $statement->bindValue(':dateStart', $_POST['dateStart'], PDO::PARAM_STR);
+    $statement->execute();
+} else {
+    // submitボタンを押さずURLを直接入力した時、日付が選択されていない時
+    session_start();
+    $_SESSION['no_dateStart'] = 'Please select the proper date.';
+}
+
+header('Location: index.php');
