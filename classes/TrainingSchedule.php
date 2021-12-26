59e895a092b5c1aad7d364816ad9b67b971e4735
hayato0309 <hayato.pluto.4538@gmail.com>
2021-12-27 00:07:58 +0900

    create background for training schedule section

---
+++ classes/TrainingSchedule.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+class TrainingSchedule
+{
+    public $monday;
+    public $tuesday;
+    public $wednesday;
+    public $thursday;
+    public $friday;
+    public $saturday;
+    public $sunday;
+
+    public function __construct(string $monday, string $tuesday, string $wednesday, string $thursday, string $friday, string $saturday, string $sunday)
+    {
+        $this->monday = $monday;
+        $this->tuesday = $tuesday;
+        $this->wednesday = $wednesday;
+        $this->thursday = $thursday;
+        $this->friday = $friday;
+        $this->saturday = $saturday;
+        $this->sunday = $sunday;
+    }
+
+    public function escape(string $trainingPart): string
+    {
+        $trainingPart = htmlspecialchars($trainingPart, ENT_HTML5 | ENT_HTML5);
+        return $trainingPart;
+    }
+
+    public function validateTrainingScheduleStrLength(string $trainingPart): bool
+    {
+        // ユーザの入力データから半角・全角スペースを削除
+        $trainingPart = str_replace(array(" ", "　"), "", $trainingPart);
+
+        // 半角・全角スペースを削除したものに対してstringの長さを測ることで、スペースのみで構成された入力データを弾く
+        if (strlen($trainingPart) <= 0 || strlen($trainingPart) > 200) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ update_training_schedule.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__FILE__) . '/connect.php';
+require_once dirname(__FILE__) . '/classes/TrainingSchedule.php';
+
+if ($_POST['submit'] === 'submit') {
+
+    $monday = $_POST['monday'];
+    $tuesday = $_POST['tuesday'];
+    $wednesday = $_POST['wednesday'];
+    $thursday = $_POST['thursday'];
+    $friday = $_POST['friday'];
+    $saturday = $_POST['saturday'];
+    $sunday = $_POST['sunday'];
+
+    $trainingSchedule = new TrainingSchedule($monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
+
+    // それぞれのトレーニング内容が1文字以上200文字以下かを確認
+    foreach ($trainingSchedule as $key => $value) {
+        $trainingPart = $trainingSchedule->escape($value);
+        $trainingPart = $trainingSchedule->validateTrainingScheduleStrLength($trainingPart);
+
+        if ($trainingPart === false) {
+            session_start();
+            $_SESSION['trainingScheduleError'] = 'Training schedule must be 1-200 characters.';
+            header('Location: index.php');
+            return;
+        }
+    }
+
+    // それぞれのトレーニング内容が1文字以上200文字以下の条件を満たした場合、以下のSQLを実行
+    $statement = $pdo->prepare('UPDATE training_schedule SET monday = :monday, tuesday = :tuesday, wednesday = :wednesday, thursday = :thursday, friday = :friday, saturday = :saturday, sunday = :sunday, created_at = CURRENT_TIMESTAMP');
+    $statement->bindValue(':monday', $_POST['monday'], PDO::PARAM_STR);
+    $statement->bindValue(':tuesday', $_POST['tuesday'], PDO::PARAM_STR);
+    $statement->bindValue(':wednesday', $_POST['wednesday'], PDO::PARAM_STR);
+    $statement->bindValue(':thursday', $_POST['thursday'], PDO::PARAM_STR);
+    $statement->bindValue(':friday', $_POST['friday'], PDO::PARAM_STR);
+    $statement->bindValue(':saturday', $_POST['saturday'], PDO::PARAM_STR);
+    $statement->bindValue(':sunday', $_POST['sunday'], PDO::PARAM_STR);
+    $statement->execute();
+} else {
+    return;
+}
+
+header('Location: index.php');
